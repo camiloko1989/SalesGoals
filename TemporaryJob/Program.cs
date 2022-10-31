2e548175d92ba12d3c088470ea85e616b4908212
Camilo Jimenez <camiloko1989@gmail.com>
2022-10-30 17:51:37 -0700

    add class Salespersone with methods

---
+++ TemporaryJob/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace TemporaryJob
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //1. Create salesperson
+            //2. Add goal to salesperson
+            //3. Show goals every month 5% 10% 15% 20%
+
+
+            void AddValues(Salesperson[] salespeople1) //fills each salesperson name and goal
+            {
+
+                foreach(Salesperson s in salespeople1)
+                {
+                    s.fullName = s.getName();
+                    s.goal = s.getGoal();
+                }
+            }
+
+            
+            Salesperson[] salespeople = new Salesperson[4] // Create an array of Salesperson objects 
+            {
+                new Salesperson(),
+                new Salesperson(),
+                new Salesperson(),
+                new Salesperson()
+            };
+
+            AddValues(salespeople); 
+
+            for (int i = 0; i < salespeople.Length; i++) //it 
+            {
+                WriteLine(salespeople[i]); //calls the object salesperson -> override ToString
+                salespeople[i].CalculateExpected(salespeople[i].goal);
+                foreach(var e in salespeople[i].expected) 
+                {
+                    WriteLine("Month #{0}: {1}", i, e); 
+                }
+                
+            }
+            ReadKey();
+        }
+    }
+}
---
+++ TemporaryJob/Salesperson.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using static System.Console;
+
+namespace TemporaryJob
+{
+    internal class Salesperson
+    {
+        public string fullName;
+        public double goal;
+        public double[] expected;
+
+
+        public Salesperson() { }
+
+        public Salesperson(string name, double goal)
+        {
+            fullName = name;
+            this.goal = goal;
+
+        }
+
+        public string getName()
+        {
+            WriteLine("Input the full name of salesperson: ");
+            fullName = ReadLine();
+            return fullName;
+        }
+
+        public double getGoal()
+        {
+            WriteLine("Input the goal for the salesperson: ");
+            goal = Convert.ToDouble(ReadLine());
+            return goal;
+        }
+
+        public string SwitchNames(string name)
+        {
+            string firstName = name.Split(' ')[0];
+            string surname = name.Split(' ')[1];
+            string switchedName = surname + ", " + firstName;
+            return switchedName;
+        }
+
+        public double[] CalculateExpected(double goal1)
+        {
+            double m1 = goal * 1.05; //increments goal in 5%
+            double m2 = m1 * 1.1;
+            double m3 = m2 * 1.15;
+            double m4 = m3 * 1.2;
+            expected = new double[] { m1, m2, m3, m4 }; //creates an array and fills it with increments
+            return expected;
+        }
+
+        /*public void ShowExpected(double[] expected1)
+        {
+            for(int i = 0; i < expected1.Length; i++)
+            {
+                WriteLine("Month #{0}: {1}", i, expected1[i]);
+            }
+        }*/
+
+        public override string ToString()
+        {
+            return "\n"+SwitchNames(fullName) + ", their goal is: " + goal + " and their expected sales are: " ;
+        }
+    }
+}
